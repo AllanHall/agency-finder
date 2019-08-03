77ffc8b8e1577e78adfc6ed3ecdf02854fb3db12
Allan Hall <bigal76126@gmail.com>
2019-08-03 14:19:08 -0400

    built out basic html and css for challenge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <div className="flex-box">
+        <div className="left-cell">
+          <div className="flexed-header">
+            <span>Input</span>
+            <span>Agency Finder</span>
+          </div>
+          <div className="search-area">
+            <label>Enter Zip Code</label>
+            <input className="input" type="search box" />
+          </div>
+          <br />
+          <button className="button">Search</button>
+        </div>
+        <div>
+          <hr width="1" size="350" />
+        </div>
+        <div className="right-cell">
+          <div>Return</div>
+          <div className="description">
+            The agency for zipcode 78532 Santa Cruz is
+          </div>
+          <table className="table">
+            <tr>
+              <td>Type</td>
+              <td>Agency</td>
+              <td>Phone Number</td>
+            </tr>
+            <tr>
+              <td>Police</td>
+              <td>Agency Name</td>
+              <td>Their Number</td>
+            </tr>
+            <tr>
+              <td>Fire</td>
+              <td>Agency Name</td>
+              <td>Their number</td>
+            </tr>
+            <tr>
+              <td>Medical</td>
+              <td>Agency Name</td>
+              <td>Their number</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    )
   }
 }
 
