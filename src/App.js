eb94ea672ae40dd03bd5b398a963f75d04980572
Allan Hall <bigal76126@gmail.com>
2019-08-04 18:27:54 -0400

    found out useEffect doesnt support async await, making new project with state

--- src/App.js
+++ src/App.js
@@ -14,15 +14,6 @@ export default function App() {
     })
   }, [])
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const result = await csv(`${data}`)
-  //     setAgencyList(result)
-  //   }
-  //   getData()
-  //   console.log(agencyList)
-  // }, [])
-
   const search = () => {
     console.log(agencyList)
     setCurrentAgencies(
@@ -32,57 +23,61 @@ export default function App() {
   }
 
   return (
-    <div className="flex-box">
-      <div className="left-cell">
-        <div className="flexed-header">
-          <span>Input</span>
-          <span>Agency Finder</span>
+    <>
+      <div className="flex-box">
+        <div className="left-cell">
+          <div className="flexed-header">
+            <span>Input</span>
+            <span>Agency Finder</span>
+          </div>
+          <div className="search-area">
+            <label>Enter Zip Code</label>
+            <input
+              className="input"
+              type="text"
+              value={searchTerm}
+              onChange={e => setSearchTerm(e.target.value)}
+            />
+          </div>
+          <br />
+          <button className="button" onClick={search}>
+            Search
+          </button>
         </div>
-        <div className="search-area">
-          <label>Enter Zip Code</label>
-          <input
-            className="input"
-            type="text"
-            value={searchTerm}
-            onChange={e => setSearchTerm(e.target.value)}
-          />
+        <div>
+          <hr width="1" size="350" />
         </div>
-        <br />
-        <button className="button" onClick={search}>
-          Search
-        </button>
+        <div className="right-cell">
+          <div>Return</div>
+          <div className="description">
+            The agency for zip_code city_name is
+          </div>
+          <table className="table">
+            <tbody>
+              <tr>
+                <td>Type</td>
+                <td>Agency</td>
+                <td>Phone Number</td>
+              </tr>
+              <tr>
+                <td>Police</td>
+                <td>lol</td>
+                <td>lol</td>
+              </tr>
+              <tr>
+                <td>Fire</td>
+                <td>agency_name</td>
+                <td>phone1</td>
+              </tr>
+              <tr>
+                <td>Medical</td>
+                <td>agency_name</td>
+                <td>phone1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div>
-        <hr width="1" size="350" />
-      </div>
-      <div className="right-cell">
-        <div>Return</div>
-        <div className="description">The agency for zip_code city_name is</div>
-        <table className="table">
-          <tbody>
-            <tr>
-              <td>Type</td>
-              <td>Agency</td>
-              <td>Phone Number</td>
-            </tr>
-            <tr>
-              <td>Police</td>
-              <td>lol</td>
-              <td>lol</td>
-            </tr>
-            <tr>
-              <td>Fire</td>
-              <td>agency_name</td>
-              <td>phone1</td>
-            </tr>
-            <tr>
-              <td>Medical</td>
-              <td>agency_name</td>
-              <td>phone1</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    </>
   )
 }
